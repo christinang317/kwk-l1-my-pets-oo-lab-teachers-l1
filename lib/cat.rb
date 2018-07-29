501db820c240c31c98e482ead42f51da411a61dc
Christina Ng <christinasy317@gmail.com>
2018-07-29 14:21:11 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,8 +1,9 @@
 class Cat
   # code goes here
   
-  attr_accessor :name, 
-  attr_reader :mood
+  attr_accessor :name, :mood 
+  
+  
   def initialize(name, mood = "nervous")
     @name = name
     @mood = mood
