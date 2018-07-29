453e097ec68da0b6e04367056795f610da73db2f
Christina Ng <christinasy317@gmail.com>
2018-07-29 14:23:11 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,21 +1,13 @@
 class Fish
   # code goes here
   
+  attr_reader :name
+  attr_accessor :mood 
+  
   def initialize(name, mood = "nervous")
     @name = name
     @mood = mood
   end
 
-  def name
-    @name
-  end
-
-  def mood=(mood)
-    @mood = mood
-  end 
-
-  def mood
-    @mood
-  end
 
 end
