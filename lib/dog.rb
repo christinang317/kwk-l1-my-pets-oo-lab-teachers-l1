1deb99957e30afb8b9b09a445bd136fa9fbd8271
Christina Ng <christinasy317@gmail.com>
2018-07-29 14:22:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,11 @@
 class Dog
   # code goes here
+  
+  attr_reader :name 
+  attr_writer :mood
   def initialize(name, mood = "nervous")
     @name = name
     @mood = mood
   end
 
-  def name
-    @name
-  end
-
-  def mood=(mood)
-    @mood = mood
-  end
-
-  def mood
-    @mood
-  end 
 end
